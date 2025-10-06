d9ba2bb0ba6eca53101ac7f60389613163aa4dff
chunyukuo88 <alexgochenour@gmail.com>
2025-10-05 20:57:29 -0400

    feat: CreateWorkout

--- store/workout_store.go
+++ store/workout_store.go
@@ -35,3 +35,23 @@ type WorkoutStore interface {
 	CreateWorkout(*Workout) (*Workout, error)
 	GetWorkoutByID(id int64) (*Workout, error)
 }
+
+func (pg *PostgresWorkoutStore) CreateWorkout(workout *Workout) (*Workout, error) {
+	tx, err := pg.db.Begin()
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback()
+
+	query :=
+		`INSERT INTO workouts (title, description, duration_minutes, calories_burned)
+		VALUES ($1, $2, $3, $4)
+
+	`
+
+	err = tx.QueryRow(query, workout.Title, workout.Description, workout.DurationMinutes, workout.CaloriesBurned).Scan(&workout.ID)
+	if err != nil {
+		return nil, err
+	}
+
+}
