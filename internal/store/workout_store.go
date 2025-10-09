fff11ca8770b879e27a5f3a6a5fc61479a40a1e6
chunyukuo88 <alexgochenour@gmail.com>
2025-10-08 21:34:01 -0400

    feat: wiring up workoutStore and workoutHandler to app.go

--- internal/store/workout_store.go
+++ internal/store/workout_store.go
@@ -74,3 +74,7 @@ func (pg *PostgresWorkoutStore) CreateWorkout(workout *Workout) (*Workout, error
 
 	return workout, nil
 }
+
+func (pg *PostgresWorkoutStore) GetWorkoutByID(id int64) (*Workout, error) {
+	return &Workout{}, nil
+}
