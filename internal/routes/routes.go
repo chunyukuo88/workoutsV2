1d9a18a105cc6d67f2be4aeb9d868f479cc9fa64
chunyukuo88 <alexgochenour@gmail.com>
2025-11-01 13:06:53 -0400

    feat: Grouping protected routes with chi

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -8,12 +8,17 @@ import (
 func SetupRoutes(app *app.Application) *chi.Mux {
 	r := chi.NewRouter()
 
-	r.Get("/health", app.HealthCheck)
-	r.Get("/workouts/{id}", app.WorkoutHandler.HandleGetByWorkoutID)
-	r.Post("/workouts", app.WorkoutHandler.HandleCreateWorkout)
-	r.Put("/workouts/{id}", app.WorkoutHandler.HandleUpdateWorkoutByID)
-	r.Delete("/workouts/{id}", app.WorkoutHandler.HandleDeleteWorkout)
+	r.Group(func(r chi.Router) {
+		r.Use(app.Middleware.Authenticate)
+
+		r.Get("/workouts/{id}", app.WorkoutHandler.HandleGetByWorkoutID)
+		r.Post("/workouts", app.WorkoutHandler.HandleCreateWorkout)
+		r.Put("/workouts/{id}", app.WorkoutHandler.HandleUpdateWorkoutByID)
+		r.Delete("/workouts/{id}", app.WorkoutHandler.HandleDeleteWorkout)
+	})
+
 	r.Post("/users", app.UserHandler.HandleRegisterUser)
+	r.Get("/health", app.HealthCheck)
 	r.Post("/tokens/authentication", app.TokenHandler.HandleCreateToken)
 	return r
 }
