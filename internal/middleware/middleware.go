a912d0ff3c8b43cc6b531e86648eb219afc21a78
chunyukuo88 <alexgochenour@gmail.com>
2025-11-01 12:52:23 -0400

    feat: Authenticate function

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -3,8 +3,11 @@ package middleware
 import (
 	"context"
 	"net/http"
+	"strings"
 
 	"github.com/chunyukuo88/workoutsV2/internal/store"
+	"github.com/chunyukuo88/workoutsV2/internal/tokens"
+	"github.com/chunyukuo88/workoutsV2/internal/utils"
 )
 
 type UserMiddleware struct {
@@ -15,7 +18,7 @@ type contextKey string
 
 const UserContextKey = contextKey("user")
 
-func SetUser(r *http.Request, user store.User) *http.Request {
+func SetUser(r *http.Request, user *store.User) *http.Request {
 	ctx := context.WithValue(r.Context(), UserContextKey, user)
 	return r.WithContext(ctx)
 }
@@ -26,4 +29,38 @@ func GetUser(r *http.Request) *store.User {
 		panic("missing user in request")
 	}
 	return user
+}
+
+func (um *UserMiddleware) Authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Vary", "Authorization")
+		authHeader := r.Header.Get("Authorization")
+
+		if authHeader == "" {
+			r = SetUser(r, store.AnonymousUser)
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		headerParts := strings.Split(authHeader, " ")
+		if len(headerParts) != 2 || headerParts[0] != "Bearer" {
+			utils.WriteJSON(w, http.StatusUnauthorized, utils.Envelope{"error": "invalid authorization header"})
+			return
+		}
+
+		token := headerParts[1]
+		user, err := um.UserStore.GetUserToken(tokens.ScopeAuth, token)
+		if err != nil {
+			utils.WriteJSON(w, http.StatusUnauthorized, utils.Envelope{"error": "invalid token"})
+			return
+		}
+
+		if user == nil {
+			utils.WriteJSON(w, http.StatusUnauthorized, utils.Envelope{"error": "token invalid or expired"})
+			return
+		}
+
+		r = SetUser(r, user)
+		next.ServeHTTP(w, r)
+	})
 }
